87829a3807b5b62919715f10b169f23e1089514d
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-07-13 22:35:19 -0300

    Adiciona o arquivo nav.sh

---
+++ nav.sh
@@ -0,0 +1,5 @@
+
+ dd(){
+     printf -v var "%s\n" $(echo "/" cheat.sheets/sheets cheatsheets RosettaCodeData/{Lang,Task} tldr/pages/{android,common,linux,osx,sunos,windows}  | xargs -n1 |fzf);
+     cd /workspace/upstream/$var
+ }
