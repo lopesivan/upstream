b94016e43985bd7fd2fa38d6626a8232c98f12a9
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-07-29 19:10:01 -0300

    Atualiza o arquivo RosettaCodeData/rosetta.sh

--- RosettaCodeData/rosetta.sh
+++ RosettaCodeData/rosetta.sh
@@ -11,3 +11,8 @@ test -d Lang/$_d || mkdir Lang/$_d
 mkdir -p Task/$Task/$Lang
 echo Task/$Task/$Lang >Task/$Task/$Lang/${arq}.${ext}
 cd Lang/$Lang && ln -s ../../Task/$Task/$Lang $Task
+
+ON=../cheat.sheets/sheets/_${Lang,,}/_info.yaml
+
+test -e $ON || echo é necessãrio criar $ON
+
