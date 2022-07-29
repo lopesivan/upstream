186bd876904ac7c330d939aebd6ff6c1cffb34a7
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-07-29 19:25:39 -0300

    Adiciona o arquivo RosettaCodeData/Lang/Ezored/android

---
+++ RosettaCodeData/Task/windows/Ezored/windows.md
@@ -0,0 +1,1 @@
+Task/windows/Ezored
--- RosettaCodeData/rosetta.sh
+++ RosettaCodeData/rosetta.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-Lang=Docker
-Task=meuTeste
-ext=dockerfile
-arq=alg42
+# Lang=Docker
+# Task=meuTeste
+# ext=dockerfile
+# arq=alg42
+
+Lang=Ezored
+#Task=linux
+#Task=android
+Task=windows
+ext=md
+arq=$Task
 # if not exist path `$Lang' then create
 _d=$Lang
 test -d Lang/$_d || mkdir Lang/$_d
@@ -14,5 +21,5 @@ cd Lang/$Lang && ln -s ../../Task/$Task/$Lang $Task
 
 ON=../cheat.sheets/sheets/_${Lang,,}/_info.yaml
 
-test -e $ON || echo é necessãrio criar $ON
+test ! -e $ON || echo é necessãrio criar $ON
 
